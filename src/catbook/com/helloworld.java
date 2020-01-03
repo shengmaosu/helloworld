cd1beae36f92453dc63ffb177745e48f604bee6c
sushengmao <sushengmao@gmail.com>
2020-01-03 16:47:57 +0800

    测试Scanner

--- src/catbook/com/helloworld.java
+++ src/catbook/com/helloworld.java
@@ -1,8 +1,17 @@
-package catbook.com;
+/**
+ * 导入
+ */
+import java.util.Scanner;
 
 public class helloworld {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner  scanner=new Scanner(System.in);
+        System.out.println("请输入你的名字");
+        String name =scanner.nextLine();
+        System.out.println("请输入你的身高");
+        String tall =scanner.nextLine();
+
     }
+
 }
